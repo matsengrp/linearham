245fc36f20e37cfb399ef2d35a419b21ac368eaa
Erick Matsen <matsen@fredhutch.org>
2016-05-13 09:35:12 -0700

    Adding Doxygen.

--- src/subroutines.cpp
+++ src/subroutines.cpp
@@ -4,29 +4,10 @@ using namespace std;
 
 
 void rowVecMatCwise(
-    Eigen::MatrixXd& A,
+    Eigen::MatrixXd& B,
     Eigen::RowVectorXd& b,
-    Eigen::MatrixXd& B) {
-  for(int i=0; i < A.rows(); i++) {
-    A.row(i) = b.cwiseProduct(B.row(i));
+    Eigen::MatrixXd& A) {
+  for(int i=0; i < B.rows(); i++) {
+    B.row(i) = b.cwiseProduct(A.row(i));
   }
 }
-
-/*
-int main()
-{
-  Eigen::MatrixXd A(3,4);
-  Eigen::RowVectorXd b(4);
-  Eigen::VectorXd c(4);
-  Eigen::MatrixXd B(3,4);
-  B <<  1, 2.9, 3, 4,
-        5, 6, 7, 8,
-        9,10,11,12;
-  b << 0, 4, 1, 10;
-
-  test(A, b, B);
-  cout << "orig\n" << B << endl;
-  cout << "final\n" << A << endl;
-
-}
-*/
--- src/subroutines.hpp
+++ src/subroutines.hpp
@@ -1,11 +1,15 @@
 #include <Eigen/Dense>
 
-//using namespace std;
-
-
+/// @brief This function takes the component-wise product of b and every row of A.
+/// @param[out] B Output matrix.
+/// @param[in]  b Input vector.
+/// @param[in]  A Input matrix.
+///  \f[
+///  B_{i,j} = b_j A_{i,j}
+///  \f]
 void rowVecMatCwise(
-    Eigen::MatrixXd& A,
+    Eigen::MatrixXd& B,
     Eigen::RowVectorXd& b,
-    Eigen::MatrixXd& B);
+    Eigen::MatrixXd& A);
 
 
