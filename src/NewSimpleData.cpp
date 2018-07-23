1f49a044afc286a683b2a585cdd8b424bcd1b12a
Amrit Dhar <adhar@uw.edu>
2018-07-23 09:31:26 -0700

    added NewSimpleData class!

---
+++ src/NewSimpleData.cpp
@@ -0,0 +1,117 @@
+#include "NewSimpleData.hpp"
+
+#include <tuple>
+
+#include "VDJGermline.hpp"
+
+/// @file NewSimpleData.cpp
+/// @brief Implementation of the NewSimpleData class.
+
+namespace linearham {
+
+
+NewSimpleData::NewSimpleData(const std::string& yaml_path,
+                             const std::string& dir_path)
+    : NewData(yaml_path, dir_path) {
+  // Parse the `input_seqs` YAML data.
+  seq_str_ = yaml_root_["events"][0]["input_seqs"][0].as<std::string>();
+  seq_ =
+      ConvertSeqToInts2(seq_str_, ggenes_.begin()->second.germ_ptr->alphabet());
+
+  // Initialize the HMM emission probability matrices.
+  InitializeHMMEmission();
+};
+
+
+// Initialization functions
+
+
+void NewSimpleData::InitializeHMMEmission() {
+  FillHMMGermlineEmission(vgerm_ggene_ranges_, vgerm_germ_inds_,
+                          vgerm_site_inds_, vgerm_emission_);
+  FillHMMJunctionEmission(vd_junction_ggene_ranges_, vd_junction_naive_bases_,
+                          vd_junction_germ_inds_, vd_junction_site_inds_,
+                          flexbounds_.at("v_r"), flexbounds_.at("d_l"),
+                          vd_junction_emission_);
+  FillHMMGermlineEmission(dgerm_ggene_ranges_, dgerm_germ_inds_,
+                          dgerm_site_inds_, dgerm_emission_);
+  FillHMMJunctionEmission(dj_junction_ggene_ranges_, dj_junction_naive_bases_,
+                          dj_junction_germ_inds_, dj_junction_site_inds_,
+                          flexbounds_.at("d_r"), flexbounds_.at("j_l"),
+                          dj_junction_emission_);
+  FillHMMGermlineEmission(jgerm_ggene_ranges_, jgerm_germ_inds_,
+                          jgerm_site_inds_, jgerm_emission_);
+};
+
+
+// Auxiliary functions
+
+
+void NewSimpleData::FillHMMGermlineEmission(
+    const std::map<std::string, std::pair<int, int>>& ggene_ranges_,
+    const std::vector<int>& germ_inds_, const std::vector<int>& site_inds_,
+    Eigen::RowVectorXd& emission_) {
+  emission_.setOnes(ggene_ranges_.size());
+
+  // Loop through the "germline" states and cache the associated HMM emission
+  // probabilities.
+  int i = 0;
+  for (auto it = ggene_ranges_.begin(); it != ggene_ranges_.end(); ++it, i++) {
+    // Obtain the (key, value) pairs from the "germline" state index map.
+    const std::string& gname = it->first;
+    int range_start, range_end;
+    std::tie(range_start, range_end) = it->second;
+
+    // Extract the "germline" state information.
+    const GermlineGene& ggene = ggenes_.at(gname);
+
+    for (int j = range_start; j < range_end; j++) {
+      emission_[i] *=
+          ggene.germ_ptr->emission()(seq_[site_inds_[j]], germ_inds_[j]);
+    }
+  }
+};
+
+
+void NewSimpleData::FillHMMJunctionEmission(
+    const std::map<std::string, std::pair<int, int>>& ggene_ranges_,
+    const std::vector<int>& naive_bases_, const std::vector<int>& germ_inds_,
+    const std::vector<int>& site_inds_, std::pair<int, int> left_flexbounds,
+    std::pair<int, int> right_flexbounds, Eigen::MatrixXd& emission_) {
+  int site_start = left_flexbounds.first;
+  int site_end = right_flexbounds.second;
+  emission_.setZero(site_end - site_start, naive_bases_.size());
+
+  // Loop through the "junction" states and cache the associated HMM emission
+  // probabilities.
+  for (auto it = ggene_ranges_.begin(); it != ggene_ranges_.end(); ++it) {
+    // Obtain the (key, value) pairs from the "junction" state index map.
+    const std::string& gname = it->first;
+    int range_start, range_end;
+    std::tie(range_start, range_end) = it->second;
+
+    // Extract the "junction" state information.
+    const GermlineGene& ggene = ggenes_.at(gname);
+
+    for (int i = range_start; i < range_end; i++) {
+      // Is the current "junction" state a NTI state?
+      if (site_inds_[i] == -1) {
+        const Eigen::MatrixXd& n_emission =
+            (ggene.type == GermlineType::D)
+                ? ggene.DGermlinePtrCast()->n_emission()
+                : ggene.JGermlinePtrCast()->n_emission();
+
+        for (int site_ind = site_start; site_ind < site_end; site_ind++) {
+          emission_(site_ind - site_start, i) =
+              n_emission(seq_[site_ind], naive_bases_[i]);
+        }
+      } else {
+        emission_(site_inds_[i] - site_start, i) =
+            ggene.germ_ptr->emission()(seq_[site_inds_[i]], germ_inds_[i]);
+      }
+    }
+  }
+};
+
+
+}  // namespace linearham
---
+++ src/NewSimpleData.hpp
@@ -0,0 +1,53 @@
+#ifndef LINEARHAM_NEWSIMPLEDATA_
+#define LINEARHAM_NEWSIMPLEDATA_
+
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <Eigen/Dense>
+#include "NewData.hpp"
+
+/// @file NewSimpleData.hpp
+/// @brief Header for the NewSimpleData class.
+
+namespace linearham {
+
+
+class NewSimpleData : public NewData {
+ private:
+  // HMM input sequence
+  Eigen::RowVectorXi seq_;
+  std::string seq_str_;
+
+  // Initialization functions
+  void InitializeHMMEmission() override;
+
+  // Auxiliary functions
+  void FillHMMGermlineEmission(
+      const std::map<std::string, std::pair<int, int>>& ggene_ranges_,
+      const std::vector<int>& germ_inds_, const std::vector<int>& site_inds_,
+      Eigen::RowVectorXd& emission_);
+
+  void FillHMMJunctionEmission(
+      const std::map<std::string, std::pair<int, int>>& ggene_ranges_,
+      const std::vector<int>& naive_bases_, const std::vector<int>& germ_inds_,
+      const std::vector<int>& site_inds_, std::pair<int, int> left_flexbounds,
+      std::pair<int, int> right_flexbounds, Eigen::MatrixXd& emission_);
+
+ public:
+  NewSimpleData(const std::string& yaml_path, const std::string& dir_path);
+
+  const Eigen::RowVectorXi& seq() const { return seq_; };
+  const std::string& seq_str() const { return seq_str_; };
+};
+
+
+typedef std::shared_ptr<NewSimpleData> NewSimpleDataPtr;
+
+
+}  // namespace linearham
+
+#endif  // LINEARHAM_NEWSIMPLEDATA_
