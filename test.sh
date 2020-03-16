10646f4c48f53682f24b5b8a7d40360d1dddf845
eharkins <eli.harkins@gmail.com>
2020-03-16 10:47:25 -0700

    update name change to --lineage-unique-ids in test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scons --run-partis --fasta-path=data/liao_dataset.fasta --all-clonal-seqs \
-    && scons --run-linearham --template-path=templates/revbayes_template.rev --mcmc-iter=25 --mcmc-thin=1 --tune-iter=0 --lineage-unique-id=KC576081.1
+    && scons --run-linearham --template-path=templates/revbayes_template.rev --mcmc-iter=25 --mcmc-thin=1 --tune-iter=0 --lineage-unique-ids=KC576081.1
